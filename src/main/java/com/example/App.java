6f5adb3b22461bdbebe2125fcfda7a979ab9a9da
_LazyDemon <jefdayt@gmail.com>
2022-04-04 21:37:52 +0200

    .

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.entityBuilder()
+                .at(200, 200)
+                .with(new AnimationComponent())
+                .buildAndAttach();
+    }
+
+    // background
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
